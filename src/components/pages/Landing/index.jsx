87d01e824c14f63ac107494208141c57f96ff2e7
Azeem Brown <azeembrown717@gmail.com>
2024-12-19 01:26:36 -0500

    1st Ticket Done

--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -1,7 +1,7 @@
 import pieChart from '../../../assets/pie-chart.png';
 import lineGraph from '../../../assets/line-graph.png';
 import barGraph from '../../../assets/bar-graph.png';
-// import paperStack from '../../../assets/paper-stack.jpg';
+import paperStack from '../../../assets/paper-stack.jpg';
 import { useNavigate } from 'react-router-dom';
 import { useDownloadData } from '../../../hooks/useDownloadData.js';
 import { decodeBase64 } from '../../../utils/decodeBase64.js';
@@ -28,25 +28,87 @@ export const LandingPage = () => {
 
   const handleReadMore = () => {
     // TODO: navigate to the humanrightsfirst.org homepage
+    window.open('https://humanrightsfirst.org', '_blank')
   };
 
   return (
-    <div className='flex-c w-[100vw] secondary-c' class="flex justify-center ">
-      <div>
-        <img src={barGraph} alt='Bar Graph' class='size-96'></img>
-        <p>Search Grant Rates By Office</p>
-      </div>
-      <div>
-        <img src={pieChart} alt='Pie Chart' class='size-96'></img>
-        <p>Search Grant Rates By Office</p>
-      </div>
-      <div>
-        <img src={lineGraph} alt='Line Graph' class='size-96'></img>
-        <p>Search Grant Rates By Office</p>
+    <body>
+      <div class='flex-c w-[100vw] secondary-c'>
+        <section class="flex primary-c pt-4 pb-8">
+          <div class="flex-c mx-auto">
+            <h1 class="text-6xl mb-8 text-white">Asylum Office Grant Rate Tracker</h1>
+            <h3 class="text-white">The Asylum Office Grant Rate Tracker provides asylum seekers, researchers, policymakers, and the public an interactive tool to explore USCIS data on Asylum Office decisions</h3>
+          </div>
+        </section>
+        <section class="graphs-section flex-c pt-10">
+          <div class="flex-c">
+            <div class="flex justify-center m-14 gap-20 text-2xl">
+              <div class="flex-c gap-3">
+                <img src={barGraph} alt='Bar Graph' class='size-96'></img>
+                <h2 class="text-3xl">Search Grant Rates By Office</h2>
+              </div>
+              <div class="flex-c gap-3">
+                <img src={pieChart} alt='Pie Chart' class='size-96 box-border p-2'></img>
+                <h2 class="text-3xl">Search Grant Rates By Nationality</h2>
+              </div>
+              <div class="flex-c gap-3">
+                <img src={lineGraph} alt='Line Graph' class='size-96 box-border p-2'></img>
+                <h2 class="text-3xl">Search Grant Rates OverTime</h2>
+              </div>
+            </div>
+            <div class="flex align-center justify-center mx-auto gap-8">
+              <button class="bg-[#aaa] px-[10px] py-[5px] text-white text-m font-semibold" onClick={() => navigate('/graphs')}>View The Data</button>
+              <button class="bg-[#aaa] px-[10px] py-[5px] text-white text-m font-semibold" onClick={downloadCSV}>Download The Data</button>
+            </div>
+          </div>
+        </section>
+        <section class="middle-section flex">
+          <div class="flex-1 hrf-img-container content-center p-20">
+            <img src={paperStack} alt='Paper Stack' class='hrf-img rounded-2xl h-[70%] w-[100%]'></img>
+          </div>
+          <div class="middle-section-text-container flex-1 content-center p-20">
+            <p class="text-xl">Human Rights First has created a search tool to give you a user-friendly way to explore a data set of asylum decisions between FY 2016 and May 2021 by the USCIS Asylum Office, which we received through a Freedom of Information Act request. You can search for information on asylum grant rates by year, nationality, and asylum office, visualize the data with charts and heat maps, and download the data set.</p>
+          </div>
+        </section>
+        <section class="insights-section flex-c gap-16 ">
+          <div class="insights-section-header">
+            <h3 class="text-5xl">Systemic Disparity Insights</h3>
+          </div>
+          <div class="insights-section-details flex justify-center m-14 gap-20 text-2xl">
+            <div class="flex-c-1 gap-12">
+              <div class="insights-details-header">
+                <h3 class="text-4xl">36%</h3>
+              </div>
+              <div class="insights-details-content">
+                <p class="text-lg">By the end of the Trump administration, the average asylum office grant rate had fallen 36% from an average of 44 percent in fiscal year 2016 to 28 percent in fiscal year 20202.</p>
+              </div>
+            </div>
+            <div class="flex-c-1 gap-12">
+              <div class="insights-details-header">
+                <h3 class="text-4xl">5%</h3>
+              </div>
+              <div class="insights-details-content">
+                <p class="text-lg">The New York asylum office grant rate dropped to 5 percent in fiscal year 2020.</p>
+              </div>
+            </div>
+            <div class="flex-c-1 gap-12">
+              <div class="insights-details-header">
+                <h3 class="text-4xl">6x Lower</h3>
+              </div>
+              <div class="insights-details-content">
+                <p class="text-lg">Between fiscal year 2017 and 2020, the New York asylum office's average grant rate was 6 times lower than the San Francisco asylum office.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="read-more-section">
+          <button class="primary-c text-white px-4 py-2" onClick={handleReadMore}>Read More</button>
+        </section>
+        <section class="back-to-top p-16">
+          <button class="back-to-top font-medium" onClick={scrollToTop}>Back To Top ^</button>
+        </section>
       </div>
 
-
-    </div>
-
+    </body>
   );
 };
