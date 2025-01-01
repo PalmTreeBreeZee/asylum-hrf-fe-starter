6f969bb1130eb7f8ffa682f13c96d1e9161b4247
Azeem Brown <azeembrown717@gmail.com>
2025-01-01 10:33:11 -0500

    almost done

--- src/components/pages/Profile/index.jsx
+++ src/components/pages/Profile/index.jsx
@@ -6,18 +6,25 @@
  * - Display the data
  * - Make this page a protected Route
  */
+import {useAuth0} from '@auth0/auth0-react'
 const Profile = () => {
   // TODO: Replace these with functionality from Auth0
-  const isLoading = false;
-  const user = true;
+ const {user, isLoading} = useAuth0()
 
   if (isLoading || !user) {
     return <div className='text-center p-4'>Loading...</div>;
   }
 
   return (
-    <div>Profile Page</div>
-  );
+      <div className='max-w-sm mx-auto mt-10 bg-white rounded-lg shadow-lg p-6 text-center'>
+
+        <img src={user.picture} className='py-pw-24 h-24 rounded-full mx-auto mb-4x' alt={`${user.name}'s profile`} />
+        <h2 className='text-gratext-2xl font-semibold mb-2y-600'>Name: {user.name}</h2>
+        <p className='text-gray-600-px'>Email: {user.email}</p>
+        
+        
+      </div>
+)
 };
 
 export default Profile;
